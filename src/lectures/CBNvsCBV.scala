5f5d596911ab3a5ec0841dbfcbf58e3a105f7238
Orlando Neto <oneto@avenuecode.com>
2021-01-07 09:43:21 -0300

    Recursion lecture

---
+++ src/lectures/CBNvsCBV.scala
@@ -0,0 +1,5 @@
+package lectures
+
+object CBNvsCBV extends  App {
+
+}
--- src/lectures/Functions.scala
+++ src/lectures/Functions.scala
@@ -1,5 +1,7 @@
 package lectures
 
+import scala.math.sqrt
+
 object Functions extends App {
 
   def aFunction(a: String, b: Int): String = {
@@ -27,6 +29,49 @@ object Functions extends App {
     def aSmallerFunction(a: Int, b: Int): Int = a + b
     aSmallerFunction(n,n-1)
   }
+  // 1. A greeting function (name, age) => "Hi, my name is $name and I am $age years old
+  def greeting(name: String, age: Int): String = {
+    "Hi, my name is " + name + " and I am " + age + " years old."
+  }
+  println(greeting("Orlando", 34))
 
-  
+//2. Factorial Functional 1*2*3*...*n
+  def factorial (n:Int): Int = {
+    if ( n <= 0)
+      1
+    else
+      n * factorial(n -1)
+  }
+  println(factorial(5))
+
+//3. fibonacci function
+  // f(1)=1
+  // f(2)=1
+  // f(n)=f(n-1) + f(n-2)
+
+  def fibonacci(n: Int): Int = {
+    if (n == 1 || n == 2)
+      1
+    else {
+      println("Computing - First I need Fibonacci of " + (n-1) + " and Fibonacci of "+ (n-2))
+      val result = fibonacci(n-1) + fibonacci(n-2)
+      println("computed factorial of " + n + ": " + result)
+      result
+    }
+  }
+  println(fibonacci(6))
+
+//4. Tests if a number is prime
+
+def isPrime(n:Int): Boolean  = {
+  def innerIsPrime (t: Int): Boolean = {
+    if (t <= 1 ) true
+    else n % t != 0 && innerIsPrime(t -1)
+  }
+  innerIsPrime(sqrt(n).toInt)
+}
+ /* println(isPrime(2))
+  println(isPrime(3))
+  println(isPrime(4))*/
+
 }
