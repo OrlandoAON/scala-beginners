c2600039977a5038e7184e0f4af89ef78b4bd9e8
Orlando Neto <oneto@avenuecode.com>
2021-01-11 19:04:20 -0300

    Call by name vs call by value and default param

--- src/lectures/CBNvsCBV.scala
+++ src/lectures/CBNvsCBV.scala
@@ -2,4 +2,18 @@ package lectures
 
 object CBNvsCBV extends  App {
 
+  def calledByValue (x:Long):Unit = {
+    println("Called by value:  "+ x);
+    println("Called by value:  "+ x);
+  }
+
+  def calledByName (x: => Long):Unit = {
+    println("Called by name:  "+ x); // the parameter is evaluated EACH time it's called, the parameter is also LAZY,...
+    println("Called by name:  "+ x);//... meaning that it'' only evaluated once it''' called
+  }
+
+  calledByValue(System.nanoTime());
+  calledByValue(System.nanoTime());
+  calledByName(System.nanoTime())
+  calledByName(System.nanoTime())
 }
