ac95c9ccc1ffebcaf35201b429cd0910b0516048
Orlando Neto <oneto@avenuecode.com>
2021-01-11 19:29:26 -0300

    string operations

--- src/lectures/DefaultArgs.scala
+++ src/lectures/DefaultArgs.scala
@@ -9,4 +9,8 @@ object DefaultArgs extends App {
 
   var fact10 = trFact(10)
 
+  def savePicture (format: String = "jpg", width: Int = 1920, height: Int = 1080):Unit = println("saving picture")
+  savePicture(width = 800); // name the parameter since all has default values
+  savePicture(height = 600, format = "bitmap", width = 800); //named can be in any order
+
 }
